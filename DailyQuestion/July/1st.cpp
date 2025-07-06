f77407b03c18bfae87ce99275d6a09e512a24736
RahulLuckwal02 <105637255+RahulLuckwal02@users.noreply.github.com>
2025-07-07 02:11:50 +0530

    1july

---
+++ DailyQuestion/July/1st.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int possibleStringCount(string word) {
+        int n = word.size();
+        int count = 0;
+
+        for(int i = 0 ; i < n ; i++){
+            if(word[i] == word[i+1])
+            count++;
+        }
+
+        return count+1;
+        
+    }
+};
+// Time Complexity: O(n), where n is the length of the string 'word'. We iterate through the string once to count adjacent pairs.
+// Space Complexity: O(1), as we use a constant amount of space for the count variable
